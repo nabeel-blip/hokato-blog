9bf62e9557bd996b28b070680a4527b266e14aaf
Nabeel <nabeelhassankhan17@gmail.com>
2022-08-16 10:34:35 +0500

    Saving a New Article
    Saving the new Article and showing the appropriate massage for the save action(errors occurred with error message's displayed or saved successfully)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,15 +7,20 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
-
+ 
     def new
-
+        @article = Article.new
     end
     
     def create
         @article = Article.new(params.require(:article).permit(:title, :description, :author))
-        @article.save
-        redirect_to  @article
+
+        if @article.save  
+           flash[:notice] = "Article has been created succesfully"
+            redirect_to  @article
+       else
+            render "new"
+       end
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "", to: "basic#home"
   get "about", to: "basic#about"
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create] 
+  end
 end
